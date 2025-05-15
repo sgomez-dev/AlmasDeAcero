5dde50ba50aba5af583c6c3137562727497816b7
EnriqueTorralboRamos <102521084+EnriqueTorralboRamos@users.noreply.github.com>
2025-05-15 16:03:59 +0200

    primer intento

--- almas-de-acero/src/pages/PruebaTuAcero.jsx
+++ almas-de-acero/src/pages/PruebaTuAcero.jsx
@@ -5,6 +5,16 @@ const PruebaTuAcero = () => {
   return (
     <div className="min-h-[calc(100vh-65px)] w-full py-8">
       <h1 className="text-4xl font-bold">Prueba tu acero</h1>
+      <div className="w-full h-screen flex justify-center items-center bg-black">
+      <iframe
+        src="/PruebaTuAcero/index.html"
+        width="960"
+        height="600"
+        title="Unity Game"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    </div>
     </div>
   );
 };
