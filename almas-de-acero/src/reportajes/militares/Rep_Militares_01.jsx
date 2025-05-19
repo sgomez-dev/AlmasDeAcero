a331a2600ce7f8a20d1b3251821670f18922ae16
AlejaandroRubio <alejandro.rubio@alumnos.uneatlantico.es>
2025-05-19 16:31:00 +0200

    Enhance content and formatting in Rep_Militares_01 component for improved readability and clarity

--- almas-de-acero/src/reportajes/militares/Rep_Militares_01.jsx
+++ almas-de-acero/src/reportajes/militares/Rep_Militares_01.jsx
@@ -31,48 +31,74 @@ export const Rep_Militares_01 = () => {
                                 D. José María Grande Urquijo ingresó al Ejército español en 1971, siguiendo los pasos de su padre. Participó en la “Marcha Verde” en 1975, fue Jefe de Inteligencia en la guerra de Bosnia-Herzegovina, y estuvo activo en los conflictos del Magreb y Oriente Medio. Además, fue agregado militar en París, jefe del departamento de comunicación del Ejército de Tierra y delegado de Defensa en Cantabria. Fuera del ámbito militar, se ha dedicado a la docencia y ha publicado numerosos artículos sobre el Islam y geopolítica.
                             </p>
 
-                            <p className="font-semibold">¿Cuáles son las diferencias fundamentales en los conflictos del Magreb y Oriente Medio?</p>
+                            <p className="font-semibold text-xl">¿Cuáles son las diferencias fundamentales en los conflictos del Magreb y Oriente Medio?</p>
                             <p className="text-justify leading-relaxed">
-                                El Magreb y Oriente Medio son teatros estratégicos diferentes, aunque ambos dominados por el islam. En el Magreb, la amenaza principal es el Sahel, donde operan grupos yihadistas como Al Qaeda. Es una zona muy pobre, lo que lleva a sus habitantes a radicalizarse o emigrar a Europa ilegalmente. En Oriente Medio, el problema radica en el conflicto con Israel, que los países árabes no reconocen como nación. Irán juega un papel clave, abasteciendo a grupos como Hezbolá y Hamas.
+                                El Magreb y Oriente Medio son dos teatros estratégicos diferentes, están todos dominados por el islam, pero son diferentes. ¿Por qué? En el Magreb no hay guerra entre comillas, porque sí la hay. Tenemos el Sahel, que es una banda de unos 100 km de ancho que separa la zona desértica al norte de la zona húmeda, boscosa al sur. Y es una zona que va desde Mauritania, Mali, Níger… hasta el mar Rojo. Es una zona muy peligrosa donde están ahora mismo refugiados Al Qaeda y Taes, donde tienen los campos de entrenamiento, donde están haciendo sus cosas, su labor de zape de captación. Una zona  muy pobre, una zona en la que el número de hijos por mujer es de 7/8 hijos. Una zona tan pobre, tan pobre que ahí hay dos soluciones o se radicalizan y se vuelven yihadistas o emigran, ¿a donde? a Europa y ahí es la emigración que estamos teniendo, inmigración ilegal, ese es un problema. En Oriente Medio el problema es que Israel es una nación que el resto de los países árabes no la toleran, no quieren que sea nación, y su objetivo es expulsarla. ¿Quién es el que maneja los hijos para esto? Irán, es la que está abasteciendo de armas, de misiles a Hezbolá, a Hamas, a la Yihad islámica. La que está propiciando los movimientos terroristas en esa zona y otras zonas del mundo.
                             </p>
 
-                            <p className="font-semibold">¿Te ha tocado intervenir en lo que ocurre entre India y Pakistán?</p>
+                            <p className="font-semibold text-xl">¿A ti en algún momento te ha tocado intervenir en lo que está ocurriendo en la India y Pakistán?</p>
                             <p className="text-justify leading-relaxed">
-                                No, porque nuestras misiones son siempre en el marco de la OTAN, incluso si están bajo mandato de Naciones Unidas. La ONU no tiene fuerzas propias, por lo que encarga las misiones a organizaciones como la OTAN o la Unión Europea.
+                                No, ahí no, porque nosotros cuando actuamos en misiones fuera siempre vamos con OTAN, que aunque la misión sea de Naciones Unidas. Naciones Unidas no tiene fuerzas entonces cuando dice, en su asamblea general o en su consejo de seguridad dicen que hay conflicto en algún lugar hay que intervenir. Como no tiene fuerzas se lo encarga a la Unión Europea o a la OTAN que sí las tienen. Entonces vas con OTAN, como yo he estado las dos veces en Bosnia bajo mandato de Naciones Unidas.
                             </p>
 
-                            <p className="font-semibold">¿La ONU puede mediar en el conflicto entre India y Pakistán, especialmente por lo nuclear?</p>
+                            <p className="font-semibold text-xl">¿La ONU puede hacer algo para parar mediar en el conflicto en India y Pakistán debido a su armamento nuclear? También mencionar que China tiene una parte llamada Aksai Chin que está creando más conflicto.</p>
                             <p className="text-justify leading-relaxed">
-                                Puede intentarlo, pero el tema nuclear es complejo. No es lo mismo que tengan armas nucleares países como Francia o EE.UU. que países como Corea del Norte o Pakistán. El riesgo de una decisión impulsiva siempre está presente.
+                                Claro, es la parte este de Cachemira, que la tienen los chinos.
+                            </p>
+                            <p className="text-justify leading-relaxed">
+                                Puede hacer pero el tema de lo nuclear es complicado, porque, todo el mundo tiene derecho a tener armas nucleares, pero claro no es lo mismo que tenga armamento nuclear Francia, Reino Unido, Estados Unidos a que lo tenga Corea, que lo tenga Pakistán.
+                            </p>
+                            <p className="text-justify leading-relaxed">
+                                Un día, se le cruzan los cables al presidente de turno, hace así en el botón y la liamos.
                             </p>
 
-                            <p className="font-semibold">¿Ves alguna similitud entre Cachemira, Rusia-Ucrania o Palestina-Israel?</p>
+                            <p className="font-semibold text-xl">¿Ve alguna similitud en lo que está ocurriendo en Cachemira, con lo que está ocurriendo en Rusia y Ucrania, o Palestina e Israel?</p>
                             <p className="text-justify leading-relaxed">
-                                Cachemira es una zona disputada por India y Pakistán debido a una mala división realizada por el Reino Unido. Palestina-Israel es un conflicto de extremismos. A pesar de intentos de paz, los radicales de ambos lados impiden acuerdos. Israel, con una historia milenaria en la región, fue reconocido como Estado en 1948 por la ONU. El conflicto palestino-israelí es profundamente complejo.
+                                No, lo del tema de Cachemira es una zona que reclaman los dos, tanto Pakistán como la India. ¿por qué la reclaman los dos? Cuando hizo la división, el Reino Unido lo hizo mal, como muchos de los problemas que tenemos hoy en día, es por culpa del Reino Unido, y al hacerlo mal los dos se consideran con el derecho a exigir esa zona de terreno.   
+                            </p>
+                            <p className='text-justify leading-relaxed'>
+                                En Palestina, el problema no es ese, el problema son los “ultras”  de un lado y del otro. Que son irreconciliables, que ha habido momentos en los que se ha estado apunto de firmar acuerdos de paz y no se firmaron, por eso, uno de los extremistas hace algo y el otro se enfada y responde. Es muy complejo el problema palestino israelí. Israel lleva viviendo en la zona desde hace 4000 años, entonces consideran que ese territorio es suyo. Cuando llegaron los romanos hubo una guerra y expulsaron a los judios, pero no se fueron todos, quedaron muchos allí. Lo que pasa que los romanos estaban hartos de los judios y quisieron hasta cambiarle el nombre y le pusieron el nombre de Palestina.
+                            </p>
+                            <p className='text-justify leading-relaxed'>
+                                La etnia Palestina no existe, osea tu puedes decir este señor es hindú y se ve, este señor es eslavo y efectivamente se le ve las características, el palestino no existe son árabes.
+                            </p>
+                            <p className='text-justify leading-relaxed'>
+                                En una guerra si una de las partes no quiere dar el brazo a torcer, indudablemente no hay acuerdo, si son las dos partes las que no están de acuerdo hay guerra, pero,  si es esas dos partes los que dominan son extremistas esas guerras además son interminables, y eso es lo que está pasando.
+                            </p>
+                            <p className='text-justify leading-relaxed'>
+                                Israel es independiente desde 1948, que lo crea Naciones Unidas. Porque el problema de los judios es que han vivido por todo el mundo como consecuencia de haberles expulsado los romanos de allí. Padecimos en europa el Holocausto, entonces a occidente  le dió verguenza de que pasase eso y no hacer nada por ellos, entonces dijo está gente se sentirán libres y seguros cuando tengan un Estado. Lo que pasa es que es un país que lleva 70 años de vida y lleva 70 años de guerra, por que no le dejan en paz. 
                             </p>
 
-                            <p className="font-semibold">¿Qué es problemático del Magreb para Europa?</p>
+
+                            <p className="font-semibold text-xl">¿Para Europa qué es problemático del Magreb?</p>
                             <p className="text-justify leading-relaxed">
-                                La inmigración. El islam radical considera inferior todo lo ajeno a su religión. Aunque muchos musulmanes no siguen esa interpretación, los yihadistas sí. En el Corán hay numerosas asoras que instan a combatir a los no creyentes.
+                                La inmigración… El problema del Islam es que es una religión iconoclasta, y es una religión que solo reconocer como bueno lo suyo, todo lo demás es malo, y como es malo yo te tengo que conquistar y te tengo que convertir a mi religión o te mato. En el Corán, se habla de que el I	slam es una religión de paz, bueno no es verdad. En el Corán hay 124 asoras que hablan de que hay que matar a los no creyentes… hay muchos musulmanes que no hacen caso de eso, evidentemente, pero hay otros que sí y esos son los yihadistas. 
                             </p>
 
-                            <p className="font-semibold">¿Qué es la Yihad?</p>
+                            <p className="font-semibold text-xl">¿Qué es la Yihad?</p>
                             <p className="text-justify leading-relaxed">
-                                Yihad significa "Guerra Santa". Mahoma hablaba de una Yihad mayor, que es la lucha interna del creyente, y una menor, que es la guerra para expandir el islam. Los salafistas dieron gran importancia a esta última, que inspira al yihadismo actual.
+                                Yihad significa Guerra Santa. Mahoma cuando predicó habló de dos tipos de yihad. El Yihad mayor es la lucha que todo buen musulmán tiene que mantener consigo mismo para ser un buen musulmán… Y el Yihad menor, es como el Islam, su objetivo es expandirse y conquistar a los no creyentes, eso es la Guerra Santa, Mahoma no le dió importancia a eso , quiénes le dieron importancia fueron los sucesores de él, fundamentalmente el salafismo y en eso es en lo que se inspiran los movimientos yihadistas.
                             </p>
 
+                            <p className="font-semibold text-xl">En los años 50, se terminó la guerra civil aquí, era un país pobre y había muchos inmigrantes. Y los inmigrantes españoles que salían a trabajar fuera, que iban a Francia, que iban a Suiza, que iban a Alemania, salían con contrato.</p>
                             <p className="text-justify leading-relaxed">
-                                En los años 50, los españoles emigraban con contratos. Hoy día, países como Ecuador, México o Colombia tienen regulaciones estrictas para entrar. Es lógico que los países protejan sus fronteras. Quien viene, debe hacerlo legalmente.
+                                Pues como ellos, ¿tú sabes lo que han tenido que firmar y todo para que le diga qué hacen? En Ecuador tienen unas restricciones excesivas. Hay gente como por ejemplo en México, que sí pueden quedar entre una semana o dos meses cada vez que quieren viajar y Colombia, que pueden quedar el tiempo que quieran, o también en Honduras.                        
+                            </p>    
+                            <p className="font-semibold text-xl">Pues sí, es que esas medidas son lógicas. Vamos a ver, todo no puede ser. Tú tienes que defender tu casa. ¿Tú permites que en tu casa entre todo el mundo?</p>
+                            <p className="text-justify leading-relaxed">
+                                Claro, no puede ser. Tú tienes que defender tus fronteras para que el que entre, entre bien. Oiga, ¿usted viene aquí a trabajar? Sí, señor, mira, aquí está mi contrato. Por ejemplo, voy a ir a trabajar a una fábrica que hay en Bilbao. ¿Y está el contrato de trabajo o de estudios, por lo menos? O de estudios, sí, sí. Bueno, digo trabajo de manera legal, no  de manera ilegal. Pero el ilegal es el que luego plantea los problemas. Eso, a años luz. 
                             </p>
-
-                            <p className="font-semibold">¿Qué opinas sobre Putin?</p>
+                            <p className="font-semibold text-xl">¿Sobre Putin qué opinas?</p>
                             <p className="text-justify leading-relaxed">
-                                Putin no busca resucitar la URSS, sino recuperar los territorios que poseía. Rusia se extiende más en Asia que en Europa. Además, nunca habla de la Unión Europea, sino de "la Europa de los pueblos" y prefiere negociaciones bilaterales.
+                                Putin lo que quiere es volver, no a la Unión Soviética, sino a tener los territorios que poseía la Unión Soviética. Porque si os fijáis en el mapa, Rusia está a caballo entre Europa y Asia.  Destacar que tiene más en Asia.
                             </p>
-
-                            <p className="font-semibold">¿Y sobre Donald Trump?</p>
+                            <p className="text-justify leading-relaxed">¿Destacar que Putin no habla de la Unión Europea, que opinas?</p>
                             <p className="text-justify leading-relaxed">
-                                Trump es predecible: dice lo que piensa sin filtros. Es empresario antes que político, y eso marca su estilo de liderazgo.
+                                Putin nunca habla de la Unión Europea. Esto es importante. Putin nunca habla de la Unión Europea. Putin siempre habla de la Europa de los pueblos. Y Putin negocia con Rumanía, negocia con Polonia, negocia con Bulgaria.. Sí, pero todos esos países pertenecen a la Unión Europea. Pero no negocia con la Unión Europea. Negocia bilateralmente. Porque bilateralmente, es más fácil que yo te convenza a ti.
+                            </p>
+                            <p className="font-semibold text-xl">En estos contextos actuales, ¿qué piensa sobre Donald Trump?</p>
+                            <p className="text-justify leading-relaxed">
+                                A Trump se le viene venir, ya sabe que a tirar algo con su bocaza, además es empresario, líder de Estados Unidos, pero empresario, que no se nos olvide.
                             </p>
                         </div>
                     </div>
